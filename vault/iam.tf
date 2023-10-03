a46168b8afa473ceff73bbc9545599f6ab5f4cae
Mr-Darlington <darlington.imade@cloudhight.com>
2023-10-03 05:43:20 +0100

    vault update

--- vault/iam.tf
+++ vault/iam.tf
@@ -1,3 +1,4 @@
+# create policy document assume role for EC2 instance
 data "aws_iam_policy_document" "assume_role" {
   statement {
     effect  = "Allow"
@@ -10,6 +11,7 @@ data "aws_iam_policy_document" "assume_role" {
   }
 }
 
+# create policy document for KMS key for vault
 data "aws_iam_policy_document" "vault-kms-unseal" {
   statement {
     sid       = "VaultKMSUnseal"
@@ -24,17 +26,20 @@ data "aws_iam_policy_document" "vault-kms-unseal" {
   }
 }
 
+#  create iam role and attach the policy document
 resource "aws_iam_role" "vault-kms-unseal" {
   name               = "vault-kms-role1"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
+#  create iam role policy and attach the policy document
 resource "aws_iam_role_policy" "vault-kms-unseal" {
   name   = "Vault-KMS-Unseal1"
   role   = aws_iam_role.vault-kms-unseal.id
   policy = data.aws_iam_policy_document.vault-kms-unseal.json
 }
 
+# create iam instance profile
 resource "aws_iam_instance_profile" "vault-kms-unseal" {
   name = "vault-kms-unseal1"
   role = aws_iam_role.vault-kms-unseal.name
