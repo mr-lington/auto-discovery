8b3bee1089adcf8c610cbe57bd45f51d4c2003cc
Mr-Darlington <darlington.imade@cloudhight.com>
2023-10-11 23:43:38 +0100

    created the playbooks

---
+++ module/ansible/prod-bashscript.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# This script automatically update ansible host inventory
+AWSBIN='/usr/local/bin/aws'
+awsDiscovery() {
+        \$AWSBIN ec2 describe-instances --filters Name=tag:aws:autoscaling:groupName,Values=euteam1-prod-asg \\
+                --query Reservations[*].Instances[*].NetworkInterfaces[*].{PrivateIpAddresses:PrivateIpAddress} > /etc/ansible/prod-ips.list
+        }
+inventoryUpdate() {
+        echo "[webservers]" > /etc/ansible/prod-hosts
+        for instance in \`cat /etc/ansible/prod-ips.list\`
+        do
+                ssh-keyscan -H \$instance >> ~/.ssh/known_hosts
+echo "\$instance ansible_user=ec2-user ansible_ssh_private_key_file=/etc/ansible/key.pem" >> /etc/ansible/prod-hosts
+       done
+}
+instanceUpdate() {
+  sleep 30
+  ansible-playbook /etc/ansible/prod-trigger.yml --extra-vars "ansible_python_interpreter=/usr/bin/python3.9"
+  sleep 30
+}
+awsDiscovery
+inventoryUpdate
+instanceUpdate
